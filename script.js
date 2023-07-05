bf18246f35c8fca70256890404e77efec693e681
vc1033 <vaishnavc9745@gmail.com>
2023-07-05 17:44:15 +0530

    Call Back Hell

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(function() {
+    document.getElementById("countdown").textContent = 10;
+    setTimeout(function() {
+      document.getElementById("countdown").textContent = 9;
+      setTimeout(function() {
+        document.getElementById("countdown").textContent = 8;
+        setTimeout(function() {
+          document.getElementById("countdown").textContent = 7;
+          setTimeout(function() {
+            document.getElementById("countdown").textContent = 6;
+            setTimeout(function() {
+              document.getElementById("countdown").textContent = 5;
+              setTimeout(function() {
+                document.getElementById("countdown").textContent = 4;
+                setTimeout(function() {
+                  document.getElementById("countdown").textContent = 3;
+                  setTimeout(function() {
+                    document.getElementById("countdown").textContent = 2;
+                    setTimeout(function() {
+                      document.getElementById("countdown").textContent = 1;
+                      setTimeout(function() {
+                        document.getElementById("countdown").textContent = "Happy Independence Day!";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
